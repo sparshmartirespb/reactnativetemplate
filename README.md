877f9ec5fef57f7643f80c6fd87addad19297547
sparsh <sparsh@purebillion.tech>
2022-11-25 18:35:25 +0530

    superbase integrated

---
+++ README.md
@@ -0,0 +1,11 @@
+npm install
+
+#Run Metro bundler
+npx react-native start --reset-cache
+
+#Make sure your device is connected
+#Runs a debug build for Android
+npx react-native run-android
+
+#Runs a debug build for IOS
+npx react-native run-ios
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component,useEffect,useState } from 'react'
 import { TouchableOpacity, Text, View, ScrollView } from 'react-native'
-
+import { createClient } from "@supabase/supabase-js";
+import { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const PostList = () => {
+  const [, setLoading] = useState(false);
+  const [text, setText] = useState([]);
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  async function getProfile() {
+    const supabaseUrl = NEXT_PUBLIC_SUPABASE_URL ;
+    const supabaseKey = NEXT_PUBLIC_SUPABASE_ANON_KEY ;
+    const options = {
+      auth: {
+        storage: AsyncStorage,
+      },
+    }
+    const supabase= createClient(supabaseUrl, supabaseKey, options);
+    try {
+      setLoading(true);
+
+      const { data, error, status } = await supabase
+        .from("dummy")
+        .select("id, text");
+      if (error && status !== 406) {
+        throw new Error("error");
+      }
+
+      if (data) {
+        console.log(data)
+        setText(data);
+      }
+    } catch (error) {
+      throw new Error("error");
+    } finally {
+      setLoading(false);
+    }
+  }
+  
   return (
     <ScrollView>
+      <Text>{text[0]?.text}</Text>
     </ScrollView>
   )
 }
